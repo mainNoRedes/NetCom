550231e5819d152dc65a8be71de04b2dc530b171
mainNoRedes <setoskay@gmail.com>
2026-08-17 12:57:31 -0600

    Add NetCom website script

---
+++ script.js
@@ -0,0 +1,1 @@
+document.addEventListener('DOMContentLoaded',()=>{document.querySelectorAll('a[href^="#"]').forEach(link=>{link.addEventListener('click',e=>{const target=document.querySelector(link.getAttribute('href'));if(target){e.preventDefault();target.scrollIntoView({behavior:'smooth'})}})})});
